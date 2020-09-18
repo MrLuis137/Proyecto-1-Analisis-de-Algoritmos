41d7886763a5e0436516531e92080b736f5683f5
MrLuis137 <47502456+MrLuis137@users.noreply.github.com>
2020-09-17 21:42:07 -0600

    Correcciones

--- Qwirkle/src/qwirkle/BoardMatrix.java
+++ Qwirkle/src/qwirkle/BoardMatrix.java
@@ -2,8 +2,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+*/
 package qwirkle;
-
 /**
  *
  * @author Luis Diego Alemán
@@ -11,16 +11,23 @@ package qwirkle;
 public class BoardMatrix {
     
     
-    private BoardMatrix board = new BoardMatrix();
-    private int width = 2;
-    private int height = 2;
+    private static BoardMatrix board = new BoardMatrix();
+    private int columns = 2;
+    private int lines = 2;
     private String[][] matrix;
     
     private BoardMatrix(){
-        matrix = new String [height][width];
+        matrix = new String [lines][columns];
+    }
+    private void fillMatrix(){
+        for(int i = 0; i < lines; i++){
+            for(int j = 0; i < columns; j++){
+                matrix[i][j] = "";
+            }
+        }
     }
     
-    public BoardMatrix getBoardMatrix(){
+    public static BoardMatrix getBoardMatrix(){
         return board;
     }
     
@@ -28,23 +35,25 @@ public class BoardMatrix {
         board = new BoardMatrix();  
     }
     
-    public void setTile(String piece, int x, int y){
-        matrix[y][x] = piece;
+    public void setTile(String piece, int line, int column){
+        matrix[line][column] = piece;
     }
     
-    public int getWidth(){
-        return width;
+    public int getLines(){
+        return columns;
     }
-    public int getHeight(){
-        return height;
+    public int getColumns(){
+        return lines;
     }
     
-    public String getTile(int x,int y){
-        return matrix[y][x]; 
+    public String getTile(int line,int column){
+        return matrix[line][column]; 
     }
     //*************************************
     //*****TO-DO Programar el resize*******
     //*************************************
     
 }
+
+
 
